ab32d4b4553a2ea78ec48c7c68cbf23d5b118f92
Sajith <sajith.hewagama@gmail.com>
2021-02-06 17:53:04 +1100

    hero video added to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Test from '../../common/Test';
+import HeroSection from './HeroSection'
 
 
  const Home = () => {
 
     return (
         <header>
-           <h3>Home</h3>
-           <Test />
+           {/* <h3>Home</h3> */}
+           {/* <Test /> */}
+           <HeroSection />
+
         </header>
     )
 }
 
 export default Home
+
