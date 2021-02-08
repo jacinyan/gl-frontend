ef5e51ba37acf93b2fff26b0f956778f5fd224ad
DK <dcsk.daniel@gmail.com>
2021-02-08 15:58:14 +1100

    changed api routes to suit netlify

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -31,7 +31,7 @@ const Login = () => {
             body: JSON.stringify(request)
         }
 
-        fetch('http://localhost:3000/api/auth/login', requestOptions)
+        fetch(process.env.REACT_APP_API_ENDPOINT + 'auth/login', requestOptions)
             .then(response => {
                 if (response.ok) {
                     return response.json();
