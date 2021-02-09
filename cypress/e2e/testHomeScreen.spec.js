0ffea8ceca0fefad72b19786cb18228daf05f3f1
Jacin <jacinjiyan@gmail.com>
2021-02-09 16:13:50 +1100

    added in propertiesscreen tests

--- cypress/e2e/testHomeScreen.spec.js
+++ cypress/e2e/testHomeScreen.spec.js
@@ -1,17 +1,21 @@
 describe("test HomeScreen", () => {
-   it("should include a nav bar and buttons", () => {
-    cy.visit("/login")
-    cy.findByTestId("loginForm").submit(); 
-    cy.get('input[name="email"]')
-        .type("admin@example.com")
-     
+    it("should include a nav bar and buttons", () => {
+        cy.visit("/login")
+        cy.findByTestId("loginForm").submit();
+        cy.get('input[name="email"]')
+            .type("admin@example.com")
+
         cy.get('input[name="password"]')
-        .type("@password")
+            .type("@password")
+        cy.findByTestId("loginForm").submit();  
 
-    cy.url().should("include", "/")
+        cy.url().should("include", "/")
+        cy.get("header")
+          .get("video")
 
-    cy.get('a[href="/home"]')
-
-
-   })
+        cy.get('a[href="/properties"]')
+          .click({force:true})
+        
+        cy.url("include", "/properties")
+    })
 })
