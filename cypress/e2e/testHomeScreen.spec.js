9f06d3c1092466296cd2a518080a2d8a788bae06
Jacin <jacinjiyan@gmail.com>
2021-02-09 15:48:27 +1100

    changed endpoint to production

--- cypress/e2e/testHomeScreen.spec.js
+++ cypress/e2e/testHomeScreen.spec.js
@@ -2,6 +2,16 @@ describe("test HomeScreen", () => {
    it("should include a nav bar and buttons", () => {
     cy.visit("/login")
     cy.findByTestId("loginForm").submit(); 
+    cy.get('input[name="email"]')
+        .type("admin@example.com")
+     
+        cy.get('input[name="password"]')
+        .type("@password")
+
     cy.url().should("include", "/")
+
+    cy.get('a[href="/home"]')
+
+
    })
 })
